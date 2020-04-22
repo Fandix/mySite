a696b8d513653aed59e913aeb0bcb8df2b11e54f
Fandix Huang <fandix0925@gamil.com>
2020-04-22 21:04:45 +0800

    new mySite Project

---
+++ src/pages/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class APP extends React.Component
+{
+    render()
+    {
+        return(
+            <div>App Component</div>
+        )
+    }
+};
+
+export default APP;
